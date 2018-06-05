2f29d02e23c3f245545c4652cb8d35c272aef684
shangzezhong <1044732267@qq.com>
2018-06-05 14:44:29 +0800

    '合并分支'

---
+++ dm-base-provider/src/main/java/cn/dm/BaseProviderLauncher.java
@@ -0,0 +1,15 @@
+package cn.dm;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class BaseProviderLauncher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseProviderLauncher.class, args);
+    }
+}
